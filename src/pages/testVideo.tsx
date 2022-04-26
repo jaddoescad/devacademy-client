e4f42ebf30a6c9d2415800d93d20f01093850ec7
Jad Slim <jadslim@Jads-MacBook-Pro.local>
2022-04-26 12:20:52 -0400

    added test video

---
+++ src/pages/testVideo.tsx
@@ -0,0 +1,22 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+interface testVideoProps {}
+
+export const testVideo: React.FC<testVideoProps> = ({}) => {
+  return (
+    <Box>
+      <iframe
+        src="https://player.vimeo.com/video/702688891?h=71abbc418c&amp;title=0&amp;byline=0&amp;portrait=0&amp;speed=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=243900"
+        width="400"
+        height="300"
+        frameBorder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowFullScreen
+        title="Untitled"
+      ></iframe>
+    </Box>
+  );
+};
+
+export default testVideo;
